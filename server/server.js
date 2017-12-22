b9575f874965f22df973e070e1d9b4ccee53c084
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-12-22 19:35:49 +0530

    Added GET request

--- server/server.js
+++ server/server.js
@@ -11,26 +11,27 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
-  // var todo = new Todo({
-  //   text : req.body.text
-  // });
   var newTodo = new Todo({
     text : req.body.text
   });
-
-  // todo.save().then((doc)=>{
-  //   res.send(doc);
-  // },(e)=>{
-  //   res.status(400).send(e);
-  // });
 
   newTodo.save().then((doc)=>{
     res.send(doc);
     console.log(doc);
     
   },(e)=>{
-    res.status(400).send("Unable to complete the request",e);
+    res.status(400).send("Unable to complete POST request",e);
   });
+});
+
+app.get('/todos', (req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({
+      todos
+    });
+  },(e)=>{
+    res.status(400).send("Unable to complete GET request!",e);
+  })
 });
 
 app.listen(3000,()=>{
