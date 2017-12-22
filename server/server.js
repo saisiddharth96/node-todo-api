c93f0077f619ff8c26b85220cfc401d5918fbc57
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-12-22 21:47:02 +0530

    Added new ID Route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -32,6 +34,25 @@ app.get('/todos', (req,res)=>{
   },(e)=>{
     res.status(400).send("Unable to complete GET request!",e);
   })
+});
+
+app.get('/todos/:id',(req,res)=>{
+var id = req.params.id;
+if(!ObjectID.isValid(id)){
+  return res.status(404).send("Unable to complete GET request!")
+}
+  
+ Todo.findById(id).then((todo)=>{
+   if(!todo){
+     return res.status(404).send();
+   }
+   res.send({todo});
+  console.log(JSON.stringify(todo,undefined,2));  
+ }).catch((e)=>{
+    res.status(400).send();
+   
+ });
+
 });
 
 app.listen(3000,()=>{
