ad8e46e3fc09f4b7a2b5aa86e3b7f8462c279b89
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-12-23 13:47:02 +0530

    Added DELETE method for /todos

--- server/server.js
+++ server/server.js
@@ -1,58 +1,82 @@
-const {ObjectID} = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+const { ObjectID } = require("mongodb");
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var {Todo} = require('./models/todos-models.js');
-var {Users} = require('./models/users-models.js');
-var {mongoose} = require('./db/mongoose.js');
+var { Todo } = require("./models/todos-models.js");
+var { Users } = require("./models/users-models.js");
+var { mongoose } = require("./db/mongoose.js");
 
 var app = express();
 var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
+app.post("/todos", (req, res) => {
   var newTodo = new Todo({
-    text : req.body.text
-  }); 
-
-  newTodo.save().then((doc)=>{
-    res.send(doc);
-    console.log(doc);
-    
-  },(e)=>{
-    res.status(400).send("Unable to complete POST request",e);
+    text: req.body.text
   });
+
+  newTodo.save().then(
+    doc => {
+      res.send(doc);
+      console.log(doc);
+    },
+    e => {
+      res.status(400).send("Unable to complete POST request", e);
+    }
+  );
 });
 
-app.get('/todos', (req,res)=>{
-  Todo.find().then((todos)=>{
-    res.send({
-      todos
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({
+        todos
+      });
+    },
+    e => {
+      res.status(400).send("Unable to complete GET request!", e);
+    }
+  );
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send("Unable to complete GET request!");
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+      console.log(JSON.stringify(todo, undefined, 2));
+    })
+    .catch(e => {
+      res.status(400).send();
     });
-  },(e)=>{
-    res.status(400).send("Unable to complete GET request!",e);
-  })
 });
 
-app.get('/todos/:id',(req,res)=>{
-var id = req.params.id;
-if(!ObjectID.isValid(id)){
-  return res.status(404).send("Unable to complete GET request!")
-}
-  
- Todo.findById(id).then((todo)=>{
-   if(!todo){
-     return res.status(404).send();
-   }
-   res.send({todo});
-  console.log(JSON.stringify(todo,undefined,2));  
- }).catch((e)=>{
-    res.status(400).send();
- });
+app.delete('/todos/:id',(req,res)=>{
+  var id = req.params.id;
 
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Unable to DELETE request!")
+  }
+
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo){
+      return res.status(400).send();
+    }
+    res.send({todo});
+    console.log(todo);
+   }).catch((e)=>{
+     console.log(`Couldnt Remove the item ${e}`);     
+     res.status(400).send();
+   });
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Server started at port ${port}`);
-  
 });
