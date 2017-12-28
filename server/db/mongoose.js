c6dde637c7637175c258bf34bee83c079cc4b7eb
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-12-28 21:12:34 +0530

    Finalized app! Everything is woking, Weird error gone

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,16 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", { useMongoClient: true });
 mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+
+//Use the below code to eliminate depreciation warning, the latest mongo client 
+//needs a callback to handle errors while starting up servers
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true } , function(err, db) {
+    if (err) {
+        console.log('Unable to connect to the server. Please start the server. Error:', err);
+    } else {
+        console.log('Connected to Server successfully!');
+    }
+});
 
 module.exports = {mongoose};
--- server/models/users-models.js
+++ server/models/users-models.js
@@ -53,7 +53,7 @@ UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = "auth";
   var token = jwt
-    .sign({ _id: user._id.toHexString(), access }, "abc123")
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_secret)
     .toString();
 
   user.tokens.push({ access, token });
@@ -76,7 +76,7 @@ UserSchema.statics.findByToken = function(token){
   var User = this;
   var decoded;
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_secret);
   } catch (e) {
     return Promise.reject();
   }
